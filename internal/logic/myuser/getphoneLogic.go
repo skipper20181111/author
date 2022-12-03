7081ecbbc1368e50c60ed8371ce54215e3148e6d
skipper20181111 <17854230845@163.com>
2022-12-03 19:58:44 +0800

    增加getphone功能

--- internal/logic/myuser/getphoneLogic.go
+++ internal/logic/myuser/getphoneLogic.go
@@ -1,7 +1,13 @@
 package myuser
 
 import (
+	"author/internal/logic/refresh"
 	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/zeromicro/go-zero/rest/httpc"
+	"io/ioutil"
+	"net/http"
 
 	"author/internal/svc"
 	"author/internal/types"
@@ -23,8 +29,21 @@ func NewGetphoneLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Getphone
 	}
 }
 
-func (l *GetphoneLogic) Getphone(req *types.LoginRes) (resp *types.LoginResp, err error) {
-	// todo: add your logic here and delete this line
+func (l *GetphoneLogic) Getphone(req *types.GetPhoneRes) (resp *types.GetPhoneResp, err error) {
+	one, err := l.svcCtx.AccessTokenModel.FindOne(l.ctx, refresh.Tockenid)
+	var getphone types.PhoneStruct
+	urlPath := fmt.Sprintf("https://api.weixin.qq.com/wxa/business/getuserphonenumber?access_token=%s", one.Token)
+	PhoneResp, err := httpc.Do(context.Background(), http.MethodPost, urlPath, getphonestruct{Code: req.PhoneCode})
+	body, _ := ioutil.ReadAll(PhoneResp.Body)
+	json.Unmarshal(body, &getphone)
+	PhoneResp.Body.Close()
+	if getphone.Errcode != 0 {
+		return &types.GetPhoneResp{Code: "4004", Msg: getphone.Errmsg}, nil
+	}
 
-	return
+	return &types.GetPhoneResp{Code: "10000", Msg: "success", Data: &types.GetPhoneRp{Phone: getphone.Phoneinfo.PurePhoneNumber, CountryCode: getphone.Phoneinfo.CountryCode}}, nil
+}
+
+type getphonestruct struct {
+	Code string `json:"code"`
 }
