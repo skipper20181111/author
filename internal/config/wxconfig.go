71b9988e435a2264e051b7431866cc4d6d1a8632
skipper20181111 <17854230845@163.com>
2023-06-29 09:38:38 +0800

    更改accesstoken刷新时间为参数化

--- internal/config/wxconfig.go
+++ internal/config/wxconfig.go
@@ -2,7 +2,8 @@ package config
 
 // 微信小程序配置
 type WxConf struct {
-	AppId      string `json:"AppId"`  //微信appId
-	Secret     string `json:"Secret"` //微信secret
-	Grant_type string `json:"Grant_type"`
+	AppId              string  `json:"AppId"`  //微信appId
+	Secret             string  `json:"Secret"` //微信secret
+	Grant_type         string  `json:"Grant_type"`
+	AccessTokenRefresh float64 `json:"AccessTokenRefresh"`
 }
